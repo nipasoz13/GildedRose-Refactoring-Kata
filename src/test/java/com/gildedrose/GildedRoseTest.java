1c5b1c025b71708e132da9493096618af92b0be2
Nicolas Pascal <nicolas.pascal@online-pajak.com>
2020-08-17 12:23:50 +1000

    Add second test case - Normal item when expired

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    @DisplayName("Should Decrease Quality By And Sellin By 1 When the Item is normal and is not expired")
+    @DisplayName("When the Item is normal and is not expired Should Decrease Quality By And Sellin By 1")
     void normalItemBeforeExpiry() {
         // Given
         String itemName = "+5 Dexterity Vest";
@@ -28,4 +28,26 @@ class GildedRoseTest {
         assertEquals(expectedQuality, app.items[0].quality);
         assertEquals(expectedSellIn, app.items[0].sellIn);
     }
+
+    @Test
+    @DisplayName("When the Item is normal and is expired Should Decrease Quality By 2 And Sellin By 1")
+    void normalItemAfterExpiry() {
+        // Given
+        String itemName = "+5 Dexterity Vest";
+        final var item = new Item(itemName, 0, 20);
+        Item[] items = new Item[]{ item };
+        GildedRose app = new GildedRose(items);
+
+        final var expectedQuality = 18;
+        final var expectedSellIn = -1;
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(itemName, app.items[0].name);
+        assertEquals(expectedQuality, app.items[0].quality);
+        assertEquals(expectedSellIn, app.items[0].sellIn);
+    }
+
 }
