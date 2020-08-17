330dfb66c4e603c49b5bd1bc66e665000ab73274
Nicolas Pascal <nicolas.pascal@online-pajak.com>
2020-08-17 12:27:46 +1000

    Test/Refacto extract constant

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,14 +8,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("UpdateQuality")
 class GildedRoseTest {
 
+    public static final String DEXTERITY_VEST_NAME = "+5 Dexterity Vest";
+
     @Test
-    @DisplayName("When the Item is normal and is not expired Should Decrease Quality By And Sellin By 1")
+    @DisplayName("When the Item is normal and is not expired It should Decrease Quality By And Sellin By 1")
     void normalItemBeforeExpiry() {
         // Given
-        String itemName = "+5 Dexterity Vest";
+        final var itemName = "+5 Dexterity Vest";
         final var item = new Item(itemName, 10, 20);
-        Item[] items = new Item[]{ item };
-        GildedRose app = new GildedRose(items);
+        final var items = new Item[]{ item };
+        final var app = new GildedRose(items);
 
         final var expectedQuality = 19;
         final var expectedSellIn = 9;
@@ -30,13 +32,13 @@ class GildedRoseTest {
     }
 
     @Test
-    @DisplayName("When the Item is normal and is expired Should Decrease Quality By 2 And Sellin By 1")
+    @DisplayName("When the Item is normal and is expired It should Decrease Quality By 2 And Sellin By 1")
     void normalItemAfterExpiry() {
         // Given
-        String itemName = "+5 Dexterity Vest";
+        final var itemName = "+5 Dexterity Vest";
         final var item = new Item(itemName, 0, 20);
-        Item[] items = new Item[]{ item };
-        GildedRose app = new GildedRose(items);
+        final var items = new Item[]{ item };
+        final var app = new GildedRose(items);
 
         final var expectedQuality = 18;
         final var expectedSellIn = -1;
@@ -50,4 +52,53 @@ class GildedRoseTest {
         assertEquals(expectedSellIn, app.items[0].sellIn);
     }
 
+    @Test
+    @DisplayName("When the Item has zero value It should Keep Quality at Zero")
+    void zeroQualityItem() {
+        // Given
+        final var dexterityVestName = DEXTERITY_VEST_NAME;
+        final var dexterityItem = new Item(dexterityVestName, 5, 0);
+        final var elixirName = "Elixir of the Mongoose";
+        final var elixirItem = new Item(elixirName, 0, 0);
+
+
+        final var items = new Item[]{ dexterityItem, elixirItem };
+        final var app = new GildedRose(items);
+
+        final var expectedQuality = 0;
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(dexterityVestName, app.items[0].name);
+        assertEquals(expectedQuality, app.items[0].quality);
+
+        assertEquals(elixirName, app.items[1].name);
+        assertEquals(expectedQuality, app.items[1].quality);
+    }
+
+    @Test
+    @DisplayName("When the Item is Aged Brie It should Increase Quality")
+    void agedBrieBeforeExpiry() {
+        // Given
+        final var itemName = "Aged Brie";
+        final var unexpiredAgedBrie = new Item(itemName, 5, 10);
+        final var expiredAgedBrie = new Item(itemName, 0, 10);
+        final var items = new Item[]{ unexpiredAgedBrie, expiredAgedBrie };
+        final var app = new GildedRose(items);
+
+        final var expectedUnexpiredBrieQuality = 11;
+        final var expectedExpiredBrieQuality = 12;
+
+        // When
+        app.updateQuality();
+
+        // Then
+        assertEquals(itemName, app.items[0].name);
+        assertEquals(expectedUnexpiredBrieQuality, app.items[0].quality);
+
+        assertEquals(itemName, app.items[1].name);
+        assertEquals(expectedExpiredBrieQuality, app.items[1].quality);
+    }
 }
